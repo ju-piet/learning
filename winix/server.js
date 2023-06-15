37ebd3493767358792679fd26d1c78253fb06d5b
ju-piet <julien.piet77@gmail.com>
2023-06-15 19:49:13 +0200

    added routes calls (users and annonces)

--- winix/server.js
+++ winix/server.js
@@ -1,6 +1,21 @@
+const mongodb = require('./database/mongodb')
 const express = require('express')
 const app = express()
 
 const PORT = 3000
+
+// Appel des routes USERS
+require('./routes/users/createUser')(app)
+require('./routes/users/findOneUser')(app)
+require('./routes/users/findAllUsers')(app)
+require('./routes/users/updateUser')(app)
+require('./routes/users/deleteUser')(app)
+
+// Appel des routes ANNONCES
+require('./routes/annonces/createAnnonce')(app)
+require('./routes/annonces/findOneAnnonce')(app)
+require('./routes/annonces/findAllAnnonce')(app)
+require('./routes/annonces/updateAnnonce')(app)
+require('./routes/annonces/deleteAnnonce')(app)
 
 app.listen(PORT, () => { console.log(`Server is running on : http://localhost:${PORT}`) })
