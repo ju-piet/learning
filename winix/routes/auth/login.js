14e6f77fde9df355256d61ea65bbe6794f67f5fc
ju-piet <julien.piet77@gmail.com>
2023-06-16 23:25:29 +0200

    created auth routes (login, auth, private key)

---
+++ winix/routes/auth/login.js
@@ -0,0 +1,34 @@
+const User = require('../../database/models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const privateKey = require('../auth/private_key')
+  
+module.exports = (app) => {
+    app.post('/api/login', (req, res) => {
+        User.findOne({ email: req.body.email})
+        .then(user => {
+            console.log("user", user)
+            if (!user) {
+                return res.status(404).json({ message: 'Utilisateur non trouvé' })
+            }
+
+            bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+                if(!isPasswordValid) {
+                    return res.status(401).json({ message: `Le mot de passe de l'utilisateur est incorrect.` })
+                }
+
+                const token = jwt.sign(
+                    { userId: user.id },
+                    privateKey,
+                    { expiresIn: '24h' })
+
+                const message = `L'utilisateur a été connecté avec succès.`
+                return res.json({ message, data: user, token })
+            })
+        })
+        .catch(error => {
+            const message = `L'utilisateur n'a pas pu se connecter`
+            return res.status(500).json({ message, data: error })
+        })
+    })
+}
