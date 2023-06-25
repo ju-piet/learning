31c7714bf5a90d3e7d83347f39d48b7c05455666
ju-piet <julien.piet77@gmail.com>
2023-06-25 17:28:08 +0200

    added validators

--- winix/app/models/usersModel.js
+++ winix/app/models/usersModel.js
@@ -3,16 +3,34 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     firstname: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: (value) => {
+                return /^[A-Za-z]+$/.test(value)
+            },
+            message: props => `${props.value} is not a valid firstname.`
+        }
     },
     lastname: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: (value) => {
+                return /^[A-Za-z]+$/.test(value)
+            },
+            message: props => `${props.value} is not a valid lastname.`
+        }
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(value)
+            },
+            message: props => `${props.value} is not a valid email.`
+        }
     },
     password: {
         type: String,
