17a2a61d1a9781364a5c8bb7f94df6d19f857547
ju-piet <julien.piet77@gmail.com>
2023-06-28 20:45:08 +0200

    rename functions

--- winix/app/routes/annoncesRoutes.js
+++ winix/app/routes/annoncesRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const auth = require('../../services/auth/auth')
+const auth = require('../service/auth')
 const annoncesController = require('../controllers/annoncesController')
 
 router.get('/', auth, annoncesController.getAllAnnonces)
 router.post('/', auth, annoncesController.createAnnonce)
 router.get('/:annonceId', auth, annoncesController.getAnnonceById)
-router.put('/:annonceId', auth, annoncesController.updateAnnonce)
-router.delete('/:annonceId', auth, annoncesController.deleteAnnonce)
+router.put('/:annonceId', auth, annoncesController.updateAnnonceById)
+router.delete('/:annonceId', auth, annoncesController.deleteAnnonceById)
 
 module.exports = router
