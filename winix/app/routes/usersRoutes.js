bf5c65576a3cf3ce18634ac709293df1c6bccd34
ju-piet <julien.piet77@gmail.com>
2023-06-28 20:45:45 +0200

    deleted function deprecated

--- winix/app/routes/usersRoutes.js
+++ winix/app/routes/usersRoutes.js
@@ -2,13 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const usersController = require('../controllers/usersController')
-const auth = require('../../services/auth/auth')
+const auth = require('../service/auth')
 
 router.get('/', auth, usersController.getAllUser)
 router.post('/', usersController.createUser)
 router.get('/:userId', auth, usersController.getUserById)
 router.put('/:userId', auth, usersController.updateUserById)
 router.delete('/:userId', auth, usersController.deleteUserById)
-router.delete('/', auth, usersController.deleteAllUser)
 
 module.exports = router
